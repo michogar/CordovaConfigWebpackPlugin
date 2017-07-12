43cd2fe384811c63c56ee0dd88aee1196affcf6f
michogar <michogar@gmail.com>
2017-07-12 18:21:33 +0200

    Created test:
    * checks if config.xml file is modified

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,50 @@
-/* globals describe, it, describe */
+/* globals describe, it, describe, afterEach */
 import { expect } from 'chai'
+import et from 'elementtree'
+const fs = require('fs')
+const path = require('path')
 
-// const CopyWebpackPlugin = require('./../dist/index')
+const CordovaConfigWebpackPlugin = require('./../dist/index')
+
+const MockCompiler = {
+  plugin: (signal, cb) => {
+    'use strict'
+    cb()
+  },
+  context: './test/helper'
+}
+
+const parseXml = (filename) => {
+  return new et.ElementTree(et.XML(fs.readFileSync(filename, 'utf-8').replace(/^\uFEFF/, '')))
+}
+
+const readXML = () => {
+  'use strict'
+  const filename = path.resolve(MockCompiler.context, 'config.xml')
+  let configXml = parseXml(filename)
+  return configXml.find('content[@src]')
+}
 
 describe('CordovaConfigWebpackPlugin', () => {
   'use strict'
-  it('Initial', () => {
-    expect(false).to.equal(true)
+  afterEach(() => {
+    const START_PAGE = 'index.html'
+    const filename = path.resolve(MockCompiler.context, 'config.xml')
+    let configXml = parseXml(filename)
+    let contentTag = configXml.find('content[@src]')
+    if (contentTag) {
+      contentTag.attrib.src = START_PAGE
+    }
+    fs.writeFileSync(filename, configXml.write({
+      indent: 4
+    }), 'utf-8')
+  })
+  it('Should modify content field in config.xml', () => {
+    const fakeOptions = {
+      page: 'fake.html'
+    }
+    const cordovaConfigWebpackPlugin = CordovaConfigWebpackPlugin(fakeOptions)
+    cordovaConfigWebpackPlugin.apply(MockCompiler)
+    expect(readXML().attrib.src).to.equal(fakeOptions.page)
   })
 })
