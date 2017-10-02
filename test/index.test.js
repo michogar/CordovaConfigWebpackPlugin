95267e11456649d59a4e5f16df0da1dc2aba00c4
michogar <michogar@gmail.com>
2017-10-02 13:32:17 +0200

    Changed to manage all config.xml parameters not only page Added changelog

--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,7 @@ import { expect } from 'chai'
 import et from 'elementtree'
 const fs = require('fs')
 const path = require('path')
+const separator = path.sep
 
 const CordovaConfigWebpackPlugin = require('./../lib/index')
 
@@ -14,43 +15,64 @@ const MockCompiler = {
   context: './test/helper'
 }
 
-const parseXml = (filename) => {
+const parseFile = () => {
+  const filename = path.resolve(MockCompiler.context, 'config.xml')
   return new et.ElementTree(et.XML(fs.readFileSync(filename, 'utf-8').replace(/^\uFEFF/, '')))
 }
 
-const readXML = () => {
-  'use strict'
-  const filename = path.resolve(MockCompiler.context, 'config.xml')
-  let configXml = parseXml(filename)
-  return configXml.find('content[@src]')
+const copyFileToTest = () => {
+  const data = fs.readFileSync(`${MockCompiler.context}${separator}test-config.xml`, 'utf-8')
+  fs.writeFileSync(`${MockCompiler.context}${separator}config.xml`, data)
 }
 
-const START_PAGE = 'index.html'
+const removeFileToTest = () => {
+  fs.unlinkSync(`${MockCompiler.context}${separator}config.xml`)
+}
 
-describe('CordovaConfigWebpackPlugin', () => {
+describe('CordovaConfigWebpackPlugin specs: ', () => {
   'use strict'
   afterEach(() => {
-    const filename = path.resolve(MockCompiler.context, 'config.xml')
-    let configXml = parseXml(filename)
-    let contentTag = configXml.find('content[@src]')
-    if (contentTag) {
-      contentTag.attrib.src = START_PAGE
-    }
-    fs.writeFileSync(filename, configXml.write({
-      indent: 4
-    }), 'utf-8')
+    removeFileToTest()
+  })
+  beforeEach(() => {
+    copyFileToTest()
   })
   it('Should modify content field in config.xml', () => {
     const fakeOptions = {
-      page: 'fake.html'
+      content: {
+        src: 'fake.html'
+      },
+      widget: {
+        version: '1.0.0'
+      },
+      author: {
+        email: 'fake@fake.com'
+      },
+      access: {
+        origin: 'fakeorigin'
+      }
     }
     const cordovaConfigWebpackPlugin = CordovaConfigWebpackPlugin(fakeOptions)
     cordovaConfigWebpackPlugin.apply(MockCompiler)
-    expect(readXML().attrib.src).to.equal(fakeOptions.page)
+    expect(parseFile().getroot().attrib.version).to.equal(fakeOptions.widget.version)
+    expect(parseFile().find('content[@src]').attrib.src).to.equal(fakeOptions.content.src)
+    expect(parseFile().find('author[@email]').attrib.email).to.equal(fakeOptions.author.email)
+    expect(parseFile().find('access[@origin]').attrib.origin).to.equal(fakeOptions.access.origin)
   })
-  it('Should run without options set', () => {
-    const cordovaConfigWebpackPlugin = CordovaConfigWebpackPlugin()
-    cordovaConfigWebpackPlugin.apply(MockCompiler)
-    expect(readXML().attrib.src).to.equal(START_PAGE)
+
+  it.only('Should launch error if not found field in config.xml', () => {
+    const fakeOptions = {
+      fake: {
+        origin: 'fakeorigin'
+      }
+    }
+    let err = false
+    try {
+      const cordovaConfigWebpackPlugin = CordovaConfigWebpackPlugin(fakeOptions)
+      cordovaConfigWebpackPlugin.apply(MockCompiler)
+    } catch (e) {
+      err = true
+    }
+    expect(err).to.equal(true)
   })
 })
